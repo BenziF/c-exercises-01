de89b5434d78fe252dc4b2cd7a1dd4be42c836a0
Federico Benzi <f.benzi.lol@gmail.com>
2018-04-10 11:35:18 +0200

    changed ex3
    Put on a while loop with an XOR, so that the PA2 bit toggles, and also used the AVR "delay" function to generate a 500ms delay at each cycle

--- HelloWorld/HelloWorld/main.c
+++ HelloWorld/HelloWorld/main.c
@@ -1,10 +1,18 @@
 
 #define F_CPU 10000000L
 #include <avr/io.h>
+#include <util/delay.h>
+#include <stdbool.h>
 
 
 int main(void)
 {
     DDRA = 0xff;            // setting all I/O's in port A as outputs
-    PORTA = ~(1<<PA2);      // turning on led 3 (active low)
+	PORTA = (1<<PA2);
+	while(1)
+	{
+		PORTA ^= (1<<PA2);
+		_delay_ms(500);
+		
+	}
 }
